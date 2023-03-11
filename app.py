332721876798222e4de605f9255381d4d953069c
Alyssa Denary <alyssa.denary@gmail.com>
2023-03-10 21:08:29 -0500

    add: test for score board and code review notes

--- app.py
+++ app.py
@@ -15,7 +15,10 @@ def homepage():
     """Show board."""
 
     # DONE: add to the test to check that correct HTML was returned
-
+    # NOTE: No game instantiation here: just table and script source, but isn't making game here
+    # Don't want home page to make game b/c home page would be get request, get requests can be cached,
+    #   cached game would be very bad
+    # Best for home page to just be html and script source
     return render_template("index.html")
 
 
@@ -43,23 +46,27 @@ def is_legal_word():
 
     # NOTE: accepts {'wordInput': 'WORD', 'gameId': gameId}
     response = request.json # <-- isn't json - it's a dict made by parsing json string
+    # FIXME: Caution name of response, b/c we are making the response.  Better: input_data
     wordInput = response["wordInput"]
     gameId = response["gameId"]
-    currentGame = games[f"{gameId}"]
+    currentGame = games[f"{gameId}"] # FIXME:  already a string
     print(currentGame.__repr__())
     print("current game")
 
     # TODO: READY - check if wordInput is in word list and on board
-    if currentGame.word_list.check_word(wordInput) and currentGame.check_word_on_board(f"{wordInput}"):
+    if (
+        currentGame.word_list.check_word(wordInput) and # FIXME: change so okay is last. Often better to fail fast and have else be winning case
+        currentGame.check_word_on_board(wordInput)
+    ):
         word_legality = "ok"
-
-    # TODO: READY - check if wordInput is not on board
-    elif not currentGame.check_word_on_board(f"{wordInput}"):
-        word_legality = "not-on-board"
 
     # TODO: READY - check if wordInput is not in word list
     elif not currentGame.word_list.check_word(wordInput):
         word_legality = "not-word"
 
-    return jsonify(f"result: {word_legality}")
+    # TODO: READY - check if wordInput is not on board
+    elif not currentGame.check_word_on_board(wordInput):
+        word_legality = "not-on-board"
+
+    return jsonify({"result": word_legality})
 
